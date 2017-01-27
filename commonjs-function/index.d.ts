10d9e962984ecc8f23bfac4c28393bdbe62c09c4
Aleksandar Djindjic <aleksandar@Aleksandars-MacBook-Pro.local>
2017-01-27 13:42:33 +0100

    commonjs export function need to be imported with requre
    Signed-off-by: Aleksandar Djindjic <aleksandar@Aleksandars-MacBook-Pro.local>

--- commonjs-function/index.d.ts
+++ commonjs-function/index.d.ts
@@ -1,1 +1,5 @@
-export = Function;
+// reference link: http://www.typescriptlang.org/docs/handbook/declaration-files/templates/module-function-d-ts.html
+
+export = MyFunction;
+
+declare function MyFunction(): string;
--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
+// reference link: http://www.typescriptlang.org/docs/handbook/declaration-files/templates/module-function-d-ts.html
+// > Note that ES6 modules cannot directly export callable functions.
+// > This file should be imported using the CommonJS-style:
+// > import x = require('someLibrary');
+
 import { func1, func2 } from './commonjs-functions-lib';
 import { mylib } from './commonjs-object';
-import * as func from './commonjs-function';
+import func = require('./commonjs-function');
 
 console.log(func1(), func2());
 console.log(mylib.func1(), mylib.func2());
