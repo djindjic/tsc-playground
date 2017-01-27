ccd8b1a59f7c9dfa6462a139e1612e0b27351a3c
Aleksandar Djindjic <aleksandar@Aleksandars-MacBook-Pro.local>
2017-01-27 12:57:17 +0100

    export = Function
    Signed-off-by: Aleksandar Djindjic <aleksandar@Aleksandars-MacBook-Pro.local>

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import { func1, func2 } from './commonjs-functions-lib';
 import { mylib } from './commonjs-object';
+import * as func from './commonjs-function';
 
 console.log(func1(), func2());
 console.log(mylib.func1(), mylib.func2());
+console.log(func());
